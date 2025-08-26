8325bba4d90cd8dee1a655b2b604fb6ae1685433
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2025-08-26 16:25:21 +0300

    Add files via upload

---
+++ distinct/distinct.sql
@@ -0,0 +1,5 @@
+--------------------- DISTINCT --------------------------------
+-- select distinct kitapAd from Kitap
+--[sadece en üstteki beyaz geceleri gösterdi ve sadece tablodaki kitapAdý sütununu gösterdi ve alfabetik sýraya göre dizdi.]
+
+select distinct KitapYayýnEvi from Kitap
