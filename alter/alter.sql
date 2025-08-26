22d0028ea4a82117e380ed4015133b28a914b43f
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2025-08-26 13:42:43 +0300

    Update alter.sql

--- alter/alter.sql
+++ alter/alter.sql
@@ -1,6 +1,9 @@
 /*-----------------  ALTER ----------------------*/
 alter table ogr add BaslamaTarihi smalldatetime, OgrMahalle varchar(50)
--- Alter ile tabloya smalldatetime veri tipinde Baslama tarihi ve varchar veri tipinde ogr mahalle sütunları ekledik
+-- Alter ile tabloya smalldatetime veri tipinde Baslama tarihi ve varchar veri tipinde ogr mahalle sÃ¼tunlarÄ± ekledik
 
 alter table ogr alter column notlar smallint
--- alter ile tablodaki notlar sütunun veri tipini int'ten smallint yaptık
+-- alter ile tablodaki notlar sÃ¼tunun veri tipini int'ten smallint yaptÄ±k
+
+alter table ogr drop column BaslamaTarihi
+-- Baslama Tarihi adÄ±ndaki sÃ¼tunu sildik
