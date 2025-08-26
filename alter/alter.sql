60c71d1131dfb78c08553c6e6bfa00d8963e96a1
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2025-08-26 13:37:38 +0300

    Add files via upload

---
+++ alter/alter.sql
@@ -0,0 +1,6 @@
+/*-----------------  ALTER ----------------------*/
+alter table ogr add BaslamaTarihi smalldatetime, OgrMahalle varchar(50)
+-- Alter ile tabloya smalldatetime veri tipinde Baslama tarihi ve varchar veri tipinde ogr mahalle sütunlarý ekledik
+
+alter table ogr alter column notlar smallint
+-- alter ile tablodaki notlar sütunun veri tipini int'ten smallint yaptýk
