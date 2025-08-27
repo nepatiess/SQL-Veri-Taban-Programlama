73d41eb9a70be81a9d4ffa4feef4fc81b425cb98
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2025-08-27 14:18:09 +0300

    Add files via upload

---
+++ Birleştirme İşlemleri/union & unıion all.sql
@@ -0,0 +1,10 @@
+---------------------UNION & UNION ALL------------------------
+-- union
+select KitapAd from Kitap
+union
+select KitapAdi from kitap2
+
+-- union all
+select KitapAd from Kitap
+union all
+select KitapAdi from kitap2
