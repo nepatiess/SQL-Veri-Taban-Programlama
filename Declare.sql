23e5e00bac96c0a4b4274c95c2d5b44e4f6af54b
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2025-09-02 15:09:49 +0300

    Add files via upload

---
+++ Declare.sql
@@ -0,0 +1,6 @@
+---------------------------DECLARE KULLANIMI-------------------
+declare @sehir varchar(15)
+declare @ilce varchar(15)
+set @sehir = 'Kahramanmaraþ'
+set @ilce = 'Elbistan'
+print @sehir + ' / ' + @ilce -- print ile sehiri ve ilceyi yazdýrdýk
