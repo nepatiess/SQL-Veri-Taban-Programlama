6609341fe52afed8e72d946b8e9a2da3f24fd700
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2025-09-02 15:07:59 +0300

    Delete Alt Sorgular directory

--- Alt Sorgular/alt sorgular.sql
+++
@@ -1,7 +0,0 @@
---------------------ALT SORGULAR----------------------------
-select * from Kitap where Vergi= (select Vergi from kitap where KitapAd = 'Çalýkuþu')
--- kitap adý çalýkuþu olan kitabýn vergisiyle eþit vergide olan kitaplarý sýraladýk
-
-select * from Kitap where KitapFiyat=(select KitapFiyat from Kitap where KitapAd = 'Son Ada')
--- Son ada kitabýnýn kitap fiyatýyla ayný olan kitaplarý listeledi
-
