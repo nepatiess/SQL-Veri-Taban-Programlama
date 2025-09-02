593e18785cb181740350750e51eecc79dc6cdc86
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2025-09-02 15:09:10 +0300

    Delete DateDiff & DateAdd directory

--- DateDiff & DateAdd/Datediff & Dateadd.sql
+++
@@ -1,8 +0,0 @@
---------------------DATE DIFF------------------------
---select DATEDIFF(Year,'1923-10-29','2016-10-29') as 'Yýl Farký', -- Date diff fonksiyonu ile ilk tarih ile ikinci tarih arasýnda kaç yýl olduðunu gösterdi
---DATEDIFF(Month, '2016-01-05', '2016-12-01') as 'Ay Farký', -- Date diff fonksiyonu ile ilk tarih ile ikinci tarih arasýnda kaç ay olduðunu gösterdi
---DATEDIFF(DAY, '2016-01-01','2016-12-01') as 'Gün Farký' -- Date diff fonksiyonu ile ilk tarih ile ikinci tarih arasýnda kaç gün olduðunu gösterdi
-
-select DATEDIFF(YEAR,'1919-05-19', GETDATE()) as 'Kaç yýl oldu' -- MKA'nýn Samsun'a çýkýþý (Gençlik spor bay.) ndan 106 yýl geçmiþ
-
-select DATEADD(day,10,GETDATE()) --Bugünün üzerine 10 gün eklendi
