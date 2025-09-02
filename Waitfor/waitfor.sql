8277fe76d7c4d5e39db750b40ac476bb2c0a8818
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2025-09-02 15:14:19 +0300

    Update and rename Waitfor/waitfor.sql to Waitfor.sql

--- Waitfor/waitfor.sql
+++
@@ -1,6 +0,0 @@
------------------------------WAIT FOR--------------------------------
-waitfor delay '00:00:10' -- 10 saniye sonra yazdýrsýn
-select '10 saniyeni boþa harcadýn :D'
-
-waitfor time '10:02:00'
-select 'þimdi de 1 dakikaný boþa yedin :)'
