7ad28588b8bffbbb626baa1069cf2a06ca31ee73
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2025-09-02 15:14:58 +0300

    Update and rename distinct/distinct.sql to Distinct.sql

--- distinct/distinct.sql
+++
@@ -1,5 +0,0 @@
---------------------- DISTINCT --------------------------------
--- select distinct kitapAd from Kitap
---[sadece en üstteki beyaz geceleri gösterdi ve sadece tablodaki kitapAdý sütununu gösterdi ve alfabetik sýraya göre dizdi.]
-
-select distinct KitapYayýnEvi from Kitap
