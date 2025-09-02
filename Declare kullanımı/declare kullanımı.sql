47bf340ae73fe9d163b46a99e42d3500ff992628
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2025-09-02 15:09:58 +0300

    Delete Declare kullanımı directory

--- Declare kullanımı/declare kullanımı.sql
+++
@@ -1,6 +0,0 @@
----------------------------DECLARE KULLANIMI-------------------
-declare @sehir varchar(15)
-declare @ilce varchar(15)
-set @sehir = 'Kahramanmaraþ'
-set @ilce = 'Elbistan'
-print @sehir + ' / ' + @ilce -- print ile sehiri ve ilceyi yazdýrdýk
